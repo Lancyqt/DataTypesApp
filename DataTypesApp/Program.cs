9eb916477f13144084c337e260fab54c76a11f65
mauricio.303268 <mauricio.303268@LAB-B-PC12>
2024-02-16 08:53:11 +0800

    Add project files.

---
+++ DataTypesApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DataTypesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the pieces of apple: ");
+            int apple;
+            apple = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter total price of " + apple + " apples: ");
+            double apple_price;
+            apple_price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The total price of apple(s) " + apple_price );
+            Console.WriteLine("The value of original price is " + apple_price);
+            Console.WriteLine("The value of converted price is " + (int) apple_price);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
